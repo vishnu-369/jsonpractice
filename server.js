204de335942da5cc1cccd7f6005fad4f4f3877ff
Vishnu Varma <JayaVishnu.Teja@247.ai>
2024-03-11 16:06:08 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const userData = [
+    {
+        "username": "vishnu",
+        "age": 25,
+        "isAdmin": true
+
+    },
+    {
+        "username": "varun",
+        "age": 35,
+        "isAdmin": false
+
+    }
+];
+
+
+const stringJson = JSON.stringify(userData)
+//console.log(stringJson)
+
+const jsonObject = JSON.parse(stringJson)
+console.log(jsonObject[0].age)
+
+
+
+
